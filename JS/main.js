d99dc82776de9420ffcc1aeafce63aca0d093bf0
hyunsic <gustlr228@hanmail.net>
2023-05-24 21:06:53 +0900

    중간수정

--- JS/main.js
+++ JS/main.js
@@ -4,21 +4,21 @@ document.querySelector("#bt01").addEventListener("click", () => {
 });
 document.querySelector("#bt02").addEventListener("click", () => {
     alert("우래옥 상세페이지로 이동합니다.");
-    location.href="https://pcmap.place.naver.com/restaurant/33068253/home?from=map&fromPanelNum=1&ts=1684898063767"
+    location.href="https://pcmap.place.naver.com/restaurant/11679381/home?from=map&fromPanelNum=2&ts=1684911991421"
 });
 document.querySelector("#bt03").addEventListener("click", () => {
     alert("고든램지 스트리트 피자 상세페이지로 이동합니다.");
-    location.href="https://pcmap.place.naver.com/restaurant/33068253/home?from=map&fromPanelNum=1&ts=1684898063767"
+    location.href="https://pcmap.place.naver.com/restaurant/1169217087/home?from=map&fromPanelNum=2&ts=1684912060494"
 });
 document.querySelector("#bt04").addEventListener("click", () => {
     alert("고씨네 천치국수 상세페이지로 이동합니다.");
-    location.href="https://pcmap.place.naver.com/restaurant/33068253/home?from=map&fromPanelNum=1&ts=1684898063767"
+    location.href="https://pcmap.place.naver.com/restaurant/1794435212/home?from=map&fromPanelNum=2&ts=1684912109798"
 });
 document.querySelector("#bt05").addEventListener("click", () => {
     alert("도토리 키친 상세페이지로 이동합니다.");
-    location.href="https://pcmap.place.naver.com/restaurant/33068253/home?from=map&fromPanelNum=1&ts=1684898063767"
+    location.href="https://pcmap.place.naver.com/restaurant/223026237/home?entry=bmp&from=map&fromPanelNum=1&ts=1684912157935"
 });
 document.querySelector("#bt06").addEventListener("click", () => {
     alert("맨도롱 해장국 상세페이지로 이동합니다.");
-    location.href="https://pcmap.place.naver.com/restaurant/33068253/home?from=map&fromPanelNum=1&ts=1684898063767"
+    location.href="https://pcmap.place.naver.com/restaurant/38711101/home?from=map&fromPanelNum=2&ts=1684912135666"
 });
